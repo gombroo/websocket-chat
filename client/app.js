5be1d88317640096173f0abc5ce2ab07e0bd6ff2
Ola Siem <info@siemieniuk.net>
2020-01-09 10:31:44 +0100

    Fix sending and receiving messages

--- client/app.js
+++ client/app.js
@@ -10,8 +10,18 @@ const socket = io(); // init new client/socket and save it in 'socket' const
 
 // socket listeners
 // socket.on('message', event => addMessage(event.author, event.content));
-socket.on('message',({ author, content }) => addMessage(author, content));
-socket.on('join' ); /// ???
+socket.on('message',({ author, content }) => { 
+  console.log(`New message from ${author}: ${content}`);
+  addMessage(author, content);
+});
+
+socket.on('join', (user) => {
+  console.log('Dołączył nowy użytkownik', user);
+});
+
+socket.on('leave', (user) => {
+  console.log('Użytkownik opuścił rozmowe: ', user);
+});
 
 // socket emitters
 socket.emit('message', { author: 'John Doe', content: 'Lorem Ipsum' });
@@ -19,15 +29,18 @@ socket.emit('message', { author: 'John Doe', content: 'Lorem Ipsum' });
 
 const loginHandler = event => {
   event.preventDefault();  
-  if(userNameInput !== null && userNameInput !== '') {
+
+  userName = userNameInput.value;
+  console.log(userName);
+
+  if(userName == null || userName == '') {
     alert('Fields can\'t be empty!');
- } else {
-    userName = userNameInput;
-    
+  } else {
     const user = socket.id; 
-    socket.emit('join', { author: userName, id: user }) // ???
+    socket.emit('join', { author: userName, id: user });
 
     messagesSection.classList.toggle('show');
+    loginForm.classList.toggle('show');
  }
 } 
 loginForm.addEventListener('submit', loginHandler);
@@ -40,6 +53,7 @@ const addMessage = (author, content) => {
 
   if(author === userName) {
     message.classList.add('message--self');
+  } else {
     message.innerHTML = `
     <h3 class="message__author">${ userName === author ? 'You': author }</h3>
     <div class="message__content">${content}</div>`;
--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const path = require('path');
-const app = express();
 const socket = require('socket.io'); // import socket.io package
+
+const app = express();
 const messages = [];
-const users = [];
+let users = [];
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname + '/client')));
@@ -17,7 +18,8 @@ app.get('*', (req, res) => {
 const server = app.listen(8000, () => {
   console.log('Server is running on Port:', 8000)
 });
-const io = socket(server); // socket.io integration with server, make server ready for communication with clients
+
+const io = socket(server); // socket.io integration with server
 
 
 // find new connection (socket) and listen to event (message), disconect after user leaves
@@ -25,21 +27,26 @@ io.on('connection', (socket) => {
   console.log('New client! Its id – ' + socket.id);
 
   socket.on('join', (user) => {
-    console.log('New user ' + socket.id + ' added!'); // ???
-    users.push(user); // ???
-    socket.broadcast.emit('user', user); // ??
-    console.log('Oh,' +  user + 'has joined the conversation!');
+    console.log('New user ' + socket.id + ' added!');
+    users.push(user); 
+    socket.broadcast.emit('user', user);
+    socket.broadcast.emit('join', user);
+    console.log('Oh, ' +  user.author + ' has joined the conversation!');
   });
 
-  // socket.on('message', () => { console.log('Oh, I\'ve got something from ' + socket.id) }); 
   socket.on('message', (message) => {
     console.log('Oh, I\'ve got something from ' + socket.id);
     messages.push(message);
     socket.broadcast.emit('message', message);
   });
 
-  socket.on('disconnect', () => { console.log('Oh, socket ' + socket.id + ' has left') });
-  console.log('I\'ve added a listener on message and disconnect events \n');
+  socket.on('disconnect', () => { 
+    const user = users.find((user) => user.id == socket.id);
+    users = users.filter(user => user.id !== socket.id);
+    socket.broadcast.emit('leave', user);
+    console.log('Oh, socket ' + socket.id + ' has left');
+    });
+  
+    //console.log('I\'ve added a listener on message and disconnect events \n');  
+  });
 
-
-});
